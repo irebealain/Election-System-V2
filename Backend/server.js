ede9726a4256334e8355cd3a05a52c874359e777
Alain Irebe Gashumba <irebalain@gmail.com>
2025-05-31 14:31:27 +0300

    Add:Frontend redirect uri

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://election-system-v2-frontend.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
