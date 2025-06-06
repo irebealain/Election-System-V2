2d9f8dd6957b27d8638f5b453cc5ec5400c8d68a
Alain Irebe Gashumba <irebalain@gmail.com>
2025-06-06 14:35:20 +0300

    Refactor ElectionStats component: adjust label dimensions and outer radius for improved visual consistency

--- frontend/src/pages/superadmin/Statistics.jsx
+++ frontend/src/pages/superadmin/Statistics.jsx
@@ -572,7 +572,7 @@ function ElectionStats() {
                                       <div className="w-9 h-9 bg-primary text-white rounded-full flex items-center justify-center shadow-lg mt-4">
                                         <Crown className="w-6 h-6" />
                                       </div>
-                                      <div className="absolute -right-1 -bottom-1 w-6 h-9 bg-yellow-400 text-white rounded-full flex items-center justify-center text-[14px] font-bold">
+                                      <div className="absolute -right-1 -bottom-1 w-6 h-6 bg-yellow-400 text-white rounded-full flex items-center justify-center text-[12px] font-bold">
                                         1st
                                       </div>
                                     </div>
@@ -602,7 +602,7 @@ function ElectionStats() {
                                           data={results}
                                           cx="50%"
                                           cy="50%"
-                                          outerRadius={55}
+                                          outerRadius={60}
                                           innerRadius={50}
                                           fill="#8884d8"
                                           dataKey="value"
